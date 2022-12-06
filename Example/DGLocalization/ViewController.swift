c4fbf7fe059ca4e3ee307fa9af6b1be4b20e2017
Devi Prasad Ghimire <devi.ghimire@mport.com.au>
2022-12-06 15:26:04 +1100

    Initial commit

---
+++ Example/DGLocalization/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DGLocalization
+//
+//  Created by Devi Prasad Ghimire on 12/06/2022.
+//  Copyright (c) 2022 Devi Prasad Ghimire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
